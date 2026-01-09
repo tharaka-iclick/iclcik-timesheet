af57eba5925717ceba2a064614688d888f9c8020
tharaka-iclick <Tharaka.Dilshan@leadlanka.lk>
2026-01-09 05:37:01 +0530

    config file update

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,56 +4,41 @@ import 'package:injectable/injectable.dart';
 
 
 abstract class AppConfig {
-  /// App API URL
   String get apiUrl;
-
-  /// Supabase ANON KEY
-  String get anonKey;
-
-  /// App Name
   String get appName;
 }
 
-/// The implementation for the development environment.
+// for the development environment.
 @Singleton(as: AppConfig, env: ['dev'])
 class DevConfig implements AppConfig {
   @override
   String get apiUrl => dotenv.env['API_URL'] ?? '';
 
   @override
-  String get anonKey => dotenv.env['ANON_KEY'] ?? '';
-
-  @override
   String get appName => dotenv.env['APP_NAME'] ?? _getDefaultAppName(AppEnv.dev);
 }
 
-/// The implementation for the QA environment.
+// for the QA environment.
 @Singleton(as: AppConfig, env: ['qa'])
 class QaConfig implements AppConfig {
   @override
   String get apiUrl => dotenv.env['API_URL'] ?? '';
 
   @override
-  String get anonKey => dotenv.env['ANON_KEY'] ?? '';
-
-  @override
   String get appName => dotenv.env['APP_NAME'] ?? _getDefaultAppName(AppEnv.qa);
 }
 
-/// The implementation for the production environment.
+// for the production environment.
 @Singleton(as: AppConfig, env: ['prod'])
 class ProdConfig implements AppConfig {
   @override
   String get apiUrl => dotenv.env['API_URL'] ?? '';
 
   @override
-  String get anonKey => dotenv.env['ANON_KEY'] ?? '';
-
-  @override
   String get appName => dotenv.env['APP_NAME'] ?? _getDefaultAppName(AppEnv.prod);
 }
 
-/// Fallback default app name based on environment
+// default app name based on environment
 String _getDefaultAppName(AppEnv env) {
   switch (env) {
     case AppEnv.dev:
